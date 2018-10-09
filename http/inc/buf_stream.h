164ecde259b6e3c51713db386c3e5e1cef12192b
高小川 <gaoxiaochuan@hotmail.com>
2018-10-09 23:28:35 +0800

    add buf stream

---
+++ http/inc/buf_stream.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "buffer_pool.h"
+#include <streambuf>
+#include <cstdint>
+#include <algorithm>
+
+namespace rwg_http {
+
+class buf_stream {
+private:
+    rwg_http::buffer& _buffer;
+    std::size_t _bpos;
+    std::size_t _pos;
+    std::size_t _epos;
+
+public:
+    buf_stream(rwg_http::buffer& buffer);
+
+    std::size_t bpos() const;
+    std::size_t pos() const;
+    std::size_t epos() const;
+
+    bool is_end() const;
+
+    std::uint8_t& getc() const;
+    void bump(std::size_t size);
+    void putc(std::uint8_t c);
+
+    template<typename _T_Output_Iter> std::size_t gets(_T_Output_Iter s, std::size_t n) {
+        auto get_size = std::min(n, this->epos() - this->pos());
+        this->_buffer.copy_to(this->_pos, this->_pos + get_size, s);
+        this->bump(get_size);
+        return get_size;
+    }
+
+    template<typename _T_Input_Iter> std::size_t puts(_T_Input_Iter begin, _T_Input_Iter end) {
+        auto put_size = this->_buffer.insert(begin, end, this->_pos);
+        this->bump(put_size);
+    }
+};
+
+}
---
+++ http/src/buf_stream.cc
@@ -0,0 +1,40 @@
+#include "buf_stream.h"
+
+rwg_http::buf_stream::buf_stream(rwg_http::buffer& buffer)
+    : _buffer(buffer)
+    , _bpos(0)
+    , _pos(0)
+    , _epos(buffer.size()) {
+
+}
+
+std::size_t rwg_http::buf_stream::bpos() const {
+    return this->_bpos;
+}
+
+std::size_t rwg_http::buf_stream::pos() const {
+    return this->_pos;
+}
+
+std::size_t rwg_http::buf_stream::epos() const {
+    return this->_epos;
+}
+
+bool rwg_http::buf_stream::is_end() const {
+    return this->_pos == this->_epos;
+}
+
+std::uint8_t& rwg_http::buf_stream::getc() const {
+    return this->_buffer[this->_pos];
+}
+
+void rwg_http::buf_stream::bump(std::size_t size) {
+    this->_pos = std::min(this->_pos + size, this->_epos);
+}
+
+void rwg_http::buf_stream::putc(std::uint8_t c) {
+    if (this->is_end()) {
+        return;
+    }
+    this->_buffer[this->_pos++] = c;
+}
