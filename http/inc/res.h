69a737afb4759371daada19513b1a311d3033522
高小川 <gaoxiaochuan@hotmail.com>
2018-10-10 22:28:09 +0800

    add basic res

---
+++ http/inc/res.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "buf_stream.h"
+#include "buffer_pool.h"
+#include <string>
+#include <map>
+#include <cstdint>
+#include <mutex>
+#include <condition_variable>
+
+namespace rwg_http {
+
+class res {
+private:
+    int _fd;
+    rwg_http::buffer _cache;
+    rwg_http::buf_stream _str;
+
+    std::string _version;
+    std::uint16_t _status_code;
+    std::string _description;
+
+    std::map<std::string, std::string> _header_parameters;
+
+    std::mutex _sync_mtx;
+    std::condition_variable _syncable_cond;
+
+    void sync();
+public:
+    res(int fd, rwg_http::buffer& buffer, rwg_http::buffer&& cache);
+
+    std::string& version();
+    std::uint16_t& status_code();
+    std::string& description();
+    std::map<std::string, std::string>& header_parameters();
+
+    void flush_header();
+};
+
+};
---
+++ http/src/res.cc
@@ -0,0 +1,57 @@
+#include "res.h"
+#include <unistd.h>
+#include <iostream>
+
+rwg_http::res::res(int fd, rwg_http::buffer& buffer, rwg_http::buffer&& cache)
+    : _fd(fd)
+    , _cache(cache)
+    , _str(buffer)
+    , _version("HTTP/1.1")
+    , _status_code(200)
+    , _description("OK") {
+
+    this->_str.set_readable_event(std::bind(&rwg_http::res::sync, this));
+}
+
+std::string& rwg_http::res::version() {
+    return this->_version;
+}
+
+std::uint16_t& rwg_http::res::status_code() {
+    return this->_status_code;
+}
+
+std::string& rwg_http::res::description() {
+    return this->_description;
+}
+
+std::map<std::string, std::string>& rwg_http::res::header_parameters() {
+    return this->_header_parameters;
+}
+
+static const std::string __crlf("\r\n");
+static const std::string __header_param_delimiter(": ");
+
+void rwg_http::res::flush_header() {
+    this->_str.write(this->_version.begin(), this->_version.size());
+    this->_str.putc(' ');
+    std::string status_code = std::to_string(this->_status_code);
+    this->_str.write(status_code.begin(), status_code.size());
+    this->_str.putc(' ');
+    this->_str.write(this->_description.begin(), this->_description.size());
+    this->_str.write(__crlf.begin(), __crlf.size());
+
+    for (auto kv : this->_header_parameters) {
+        this->_str.write(kv.first.begin(), kv.first.size());
+        this->_str.write(__header_param_delimiter.begin(), __header_param_delimiter.size());
+        this->_str.write(kv.second.begin(), kv.second.size());
+        this->_str.write(__crlf.begin(), __crlf.size());
+    }
+    this->_str.write(__crlf.begin(), __crlf.size());
+}
+
+void rwg_http::res::sync() {
+    std::size_t n = this->_str.read(this->_cache.unit(0), this->_cache.size());
+    ::write(this->_fd, this->_cache.unit(0), n);
+}
+
