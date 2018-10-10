ac7ee5aec34cf0498fa60af4280358663a0f5d04
高小川 <gaoxiaochuan@hotmail.com>
2018-10-10 15:04:18 +0800

    add buf_stream use/unuse cond && add load method (in stream use it)

--- http/inc/buf_stream.h
+++ http/inc/buf_stream.h
@@ -4,6 +4,8 @@
 #include <streambuf>
 #include <cstdint>
 #include <algorithm>
+#include <mutex>
+#include <condition_variable>
 
 namespace rwg_http {
 
@@ -14,6 +16,13 @@ private:
     std::size_t _pos;
     std::size_t _epos;
 
+    std::mutex _use_mtx;
+    std::condition_variable _useable_cond;
+    std::condition_variable _unuseable_cond;
+
+    void bump(std::size_t size);
+    void wait_useable(std::unique_lock<std::mutex>& locker);
+    void wait_unuseable(std::unique_lock<std::mutex>& locker);
 public:
     buf_stream(rwg_http::buffer& buffer);
 
@@ -24,11 +33,13 @@ public:
     bool is_end() const;
     void clear();
 
-    std::uint8_t& getc() const;
-    void bump(std::size_t size);
+    std::uint8_t getc();
     void putc(std::uint8_t c);
 
     template<typename _T_Output_Iter> std::size_t gets(_T_Output_Iter s, std::size_t n) {
+        std::unique_lock<std::mutex> locker(this->_use_mtx);
+        this->wait_useable(locker);
+
         auto get_size = std::min(n, this->epos() - this->pos());
         this->_buffer.copy_to(this->_pos, this->_pos + get_size, s);
         this->bump(get_size);
@@ -36,9 +47,32 @@ public:
     }
 
     template<typename _T_Input_Iter> std::size_t puts(_T_Input_Iter begin, _T_Input_Iter end) {
+        std::unique_lock<std::mutex> locker(this->_use_mtx);
+        this->wait_useable(locker);
+
         auto put_size = this->_buffer.insert(begin, end, this->_pos);
         this->bump(put_size);
         return put_size;
+    }
+
+    // use for in stream
+    template<typename _T_Input_Iter> std::size_t load(_T_Input_Iter begin, _T_Input_Iter end) {
+        std::unique_lock<std::mutex> locker(this->_use_mtx);
+
+        std::size_t size;
+        if (this->is_end()) {
+            size = this->_buffer.assign(begin, end);
+            this->_bpos = 0;
+            this->_pos = 0;
+            this->_epos = size;
+        }
+        else {
+            size = this->_buffer.insert(begin, end, this->_epos);
+            this->_epos += size;
+        }
+
+        this->_useable_cond.notify_one();
+        return size;
     }
 };
 
--- http/src/buf_stream.cc
+++ http/src/buf_stream.cc
@@ -4,7 +4,7 @@ rwg_http::buf_stream::buf_stream(rwg_http::buffer& buffer)
     : _buffer(buffer)
     , _bpos(0)
     , _pos(0)
-    , _epos(buffer.size()) {
+    , _epos(0) {
 
     this->_buffer.fill(0, this->_epos, 0x00);
 }
@@ -26,20 +26,43 @@ bool rwg_http::buf_stream::is_end() const {
 }
 
 void rwg_http::buf_stream::clear() {
-    this->_buffer.fill(this->_bpos, this->_epos, 0x00);
+    std::unique_lock<std::mutex> locker(this->_use_mtx);
+    this->wait_unuseable(locker);
+
+    this->_bpos = 0;
+    this->_pos = 0;
+    this->_epos = this->_buffer.size();
+    this->_useable_cond.notify_one();
 }
 
-std::uint8_t& rwg_http::buf_stream::getc() const {
-    return this->_buffer[this->_pos];
+std::uint8_t rwg_http::buf_stream::getc() {
+    std::unique_lock<std::mutex> locker(this->_use_mtx);
+    this->wait_useable(locker);
+
+    std::uint8_t c = this->_buffer[this->_pos];
+    this->bump(1);
+    return c;
 }
 
 void rwg_http::buf_stream::bump(std::size_t size) {
     this->_pos = std::min(this->_pos + size, this->_epos);
+    this->_unuseable_cond.notify_one();
+}
+
+void rwg_http::buf_stream::wait_useable(std::unique_lock<std::mutex>& locker) {
+    this->_useable_cond.wait(locker, [this] () -> bool { return !this->is_end(); });
+}
+
+void rwg_http::buf_stream::wait_unuseable(std::unique_lock<std::mutex>& locker) {
+    this->_unuseable_cond.wait(locker, [this] () -> bool { return this->is_end(); });
+
 }
 
 void rwg_http::buf_stream::putc(std::uint8_t c) {
-    if (this->is_end()) {
-        return;
-    }
-    this->_buffer[this->_pos++] = c;
+    std::unique_lock<std::mutex> locker(this->_use_mtx);
+    this->wait_useable(locker);
+
+    this->_buffer[this->_pos] = c;
+    this->bump(1);
 }
+
