9da0392e165f6b7c4e9259aac01af49503698936
高小川 <gaoxiaochuan@hotmail.com>
2018-10-14 20:45:27 +0800

    test server

--- http/test/server_test.cc
+++ http/test/server_test.cc
@@ -8,9 +8,20 @@ int main() {
     server.init_buffer_pool(128);
 
     auto middleware = [] (rwg_http::session& sess) -> bool {
-        sess.req().get_header();
         std::cout << sess.req().uri() << std::endl;
 
+        sess.res().status_code() = 400;
+        sess.res().description() = "Bad Request";
+        sess.res().header_parameters()["Connection"] = "close";
+        sess.res().header_parameters()["Content-Type"] = "text/html";
+        std::string html = "<h1>Bad Request</h1>\r\n\r\n";
+        sess.res().header_parameters()["Content-Length"] = std::to_string(html.size());
+
+        sess.res().write_header();
+        sess.res().write(html.c_str(), html.size());
+        sess.res().flush();
+
+        sess.close();
         return false;
     };
 
