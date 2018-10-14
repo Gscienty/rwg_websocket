9b511a2740d8738ddf249c5b0a7ccbce8b23f3aa
高小川 <gaoxiaochuan@hotmail.com>
2018-10-14 20:44:20 +0800

    add keep-connection

--- http/src/chain_middleware.cc
+++ http/src/chain_middleware.cc
@@ -5,12 +5,28 @@ void rwg_http::chain_middleware::append(std::function<bool (rwg_http::session&)>
 }
 
 void rwg_http::chain_middleware::execute(rwg_http::session& session) const {
-    for (auto middleware_func : this->_middlewares) {
-        if (session.closed_flag()) {
-            break;
+    while (!session.closed_flag()) {
+        session.req().clear();
+        session.res().clear();
+
+        session.req().get_header();
+        
+        bool handled = false;
+        for (auto middleware_func : this->_middlewares) {
+            if (session.closed_flag()) {
+                break;
+            }
+            if (!middleware_func(session)) {
+                handled = true;
+                break;
+            }
         }
-        if (!middleware_func(session)) {
-            break;
+
+        if (handled) {
+            session.res().status_code() = 400;
+            session.res().description() = "Bad Request";
+            session.res().write_header();
+            session.res().flush();
         }
     }
 }
