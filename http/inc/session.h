565138435df41549bdb69b9087e8e4239c3d3224
高小川 <gaoxiaochuan@hotmail.com>
2018-10-14 18:49:04 +0800

    add session close, req reading flag && add epoll event

--- http/inc/session.h
+++ http/inc/session.h
@@ -4,21 +4,41 @@
 #include "req.h"
 #include "res.h"
 #include <memory>
+#include <atomic>
+#include <sys/epoll.h>
+#include <atomic>
 
 namespace rwg_http {
 
 class session {
 private:
     int _fd;
-    std::unique_ptr<rwg_http::req> _req;
-    std::unique_ptr<rwg_http::res> _res;
+    std::shared_ptr<rwg_http::req> _req;
+    std::shared_ptr<rwg_http::res> _res;
 
     rwg_http::buffer_pool& _pool;
+
+    std::function<void ()> _req_close;
+    std::atomic<bool> _closed_flag;
+
+    std::function<void ()> _close_callback;
+
+    ::epoll_event _event;
+    std::atomic<bool> _reading;
 public:
-    session(int fd, rwg_http::buffer_pool& pool, std::function<void (rwg_http::buf_outstream&)> notify_func);
+    session(int fd,
+            rwg_http::buffer_pool& pool,
+            std::function<void (rwg_http::buf_outstream&)> need_outsync_func,
+            std::function<void ()> close_callback);
 
     rwg_http::req& req();
     rwg_http::res& res();
+
+    std::atomic<bool>& closed_flag();
+    void close();
+
+    ::epoll_event& event() { return this->_event; }
+    std::atomic<bool>& reading() { return this->_reading; }
 };
 
 }
--- http/src/session.cc
+++ http/src/session.cc
@@ -1,11 +1,27 @@
 #include "session.h"
+#include <iostream>
 
-rwg_http::session::session(int fd, rwg_http::buffer_pool& pool, std::function<void (rwg_http::buf_outstream&)> notify_func)
+rwg_http::session::session(int fd,
+                           rwg_http::buffer_pool& pool,
+                           std::function<void (rwg_http::buf_outstream&)> need_outsync_func,
+                           std::function<void ()> close_callback)
     : _fd(fd)
-    , _pool(pool) {
+    , _req(std::make_shared<rwg_http::req>(fd, pool.alloc(32), std::bind(&rwg_http::session::close, this)))
+    , _res(std::make_shared<rwg_http::res>(fd, pool.alloc(32), need_outsync_func, std::bind(&rwg_http::session::close, this)))
+    , _pool(pool)
+    , _closed_flag(false)
+    , _close_callback(close_callback)
+    , _reading(false) {
+}
 
-    this->_req.reset(new rwg_http::req(fd, pool.alloc(32)));
-    this->_res.reset(new rwg_http::res(fd, pool.alloc(32), notify_func));
+void rwg_http::session::close() {
+    if (this->_closed_flag) {
+        return;
+    }
+    this->_closed_flag = true;
+    this->_req->close();
+    this->_res->close();
+    this->_close_callback();
 }
 
 rwg_http::req& rwg_http::session::req() {
@@ -15,4 +31,9 @@ rwg_http::req& rwg_http::session::req() {
 rwg_http::res& rwg_http::session::res() {
     return *this->_res;
 }
+
+std::atomic<bool>& rwg_http::session::closed_flag() {
+    return this->_closed_flag;
+}
+
 
