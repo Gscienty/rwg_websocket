af8fae43dd3906433934604f56daf50c8f17062c
高小川 <gaoxiaochuan@hotmail.com>
2018-10-14 18:46:35 +0800

    change chain append middleware method's name

--- http/inc/chain_middleware.h
+++ http/inc/chain_middleware.h
@@ -11,13 +11,14 @@ private:
     std::list<std::function<bool (rwg_http::session&)>> _middlewares;
 
 public:
-    void append(std::function<bool (rwg_http::session&)> middleware);
 
     template<typename _T_Middleware>
-    void append(_T_Middleware& middleware) {
+    void append_middleware(_T_Middleware& middleware) {
         this->_middlewares
             .push_back(std::bind(&_T_Middleware::run, &middleware, std::placeholders::_1));
     }
+    
+    void append(std::function<bool (rwg_http::session&)> middleware);
 
     void execute(rwg_http::session& session) const;
 };
