b7db023fdf89f813c7a06ad1398dc36744c53ff5
高小川 <gaoxiaochuan@hotmail.com>
2018-10-17 13:48:12 +0800

    test staticfile

--- staticfile/test/server.cc
+++ staticfile/test/server.cc
@@ -3,10 +3,10 @@
 #include <iostream>
 
 int main() {
-    rwg_http::server server(16);
+    rwg_http::server server(1);
 
-    server.init_buffer_pool(4096);
-    server.init_thread_pool(16);
+    server.init_buffer_pool(128);
+    server.init_thread_pool(2);
 
     rwg_staticfile::startup staticfile("/static/", "./test/");
     staticfile.read_config("./mime.conf");
@@ -16,12 +16,20 @@ int main() {
         return true;
     };
 
-    server.chain().append(func);
+    auto func_end = [] (rwg_http::session& session) -> bool {
+        session.close();
+        std::cout << "CLOSE\n";
+        return false;
+    };
 
+    server.chain().append(func);
     server.chain().append_middleware(staticfile);
+    server.chain().append(func_end);
 
     server.listen("0.0.0.0", 5000);
+#ifdef DEBUG
     std::cout << "RUNNING\n";
+#endif
     server.start(16, -1);
 
     return 0;
