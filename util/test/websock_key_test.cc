b69c2a87542fe96df5502650bc7ca0b2c0e3c45b
高小川 <gaoxiaochuan@hotmail.com>
2018-10-22 22:57:20 +0800

    test websock

--- util/test/websock_key_test.cc
+++ util/test/websock_key_test.cc
@@ -1,12 +1,13 @@
-#include "base64.hpp"
-#include "sha1.hpp"
+#include "util/base64.hpp"
+#include "util/sha1.hpp"
 
 #include <string>
 #include <iostream>
 #include <iomanip>
 
 int main() {
-    std::string key = "dGhlIHNhbXBsZSBub25jZQ==";
+    /* std::string key = "dGhlIHNhbXBsZSBub25jZQ=="; */
+    std::string key = "sN9cRrP/n9NdMgdcy2VJFQ==";
     std::string uid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
     std::basic_string<uint8_t> p(key.begin(), key.end());
