fa203b1ffeade2f95221ca4c25808f51ee7611c1
高小川 <gaoxiaochuan@hotmail.com>
2018-10-22 22:56:52 +0800

    test staticfile

--- staticfile/test/server.cc
+++ staticfile/test/server.cc
@@ -1,5 +1,5 @@
-#include "server.hpp"
-#include "staticfile_startup.hpp"
+#include "web/server.hpp"
+#include "staticfile/staticfile_startup.hpp"
 #include <iostream>
 
 int main() {
@@ -7,9 +7,12 @@ int main() {
     staticfile.read_config("./mime.conf");
 
     rwg_web::server server(10, 10, 2000);
-    server.listen("127.0.0.1", 8088);
+    server.listen("0.0.0.0", 5000);
 
     auto func = [&] (rwg_web::req& req, rwg_web::res& res) -> void {
+        req.alloc_buf(512);
+        res.alloc_buf(512);
+        req.parse();
         staticfile.run(req, res);
     };
 
