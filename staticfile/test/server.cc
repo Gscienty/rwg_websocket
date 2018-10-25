5c1c48401171343f24916fcd3b56e156b36a72e0
高小川 <gaoxiaochuan@hotmail.com>
2018-10-25 18:49:11 +0800

    test staticfile

--- staticfile/test/server.cc
+++ staticfile/test/server.cc
@@ -3,20 +3,18 @@
 #include <iostream>
 
 int main() {
-    rwg_staticfile::startup staticfile("/static/", "./");
-    staticfile.read_config("./mime.conf");
+    rwg_staticfile::startup staticfile("/static/", "staticfile/test/");
+    staticfile.read_config("staticfile/mime.conf");
 
     rwg_web::server server(10, 10, 2000);
     server.listen("0.0.0.0", 5000);
 
     auto func = [&] (rwg_web::req& req, rwg_web::res& res) -> void {
-        req.alloc_buf(512);
-        res.alloc_buf(512);
-        req.parse();
+        std::cout << "staticfile" << std::endl;
         staticfile.run(req, res);
     };
 
-    server.run(func);
+    server.http_handle(func);
     server.start();
     return 0;
 }
