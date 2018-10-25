37ff829e96220f1d96b2b1fd822b1487892a2473
高小川 <gaoxiaochuan@hotmail.com>
2018-10-25 18:46:11 +0800

    change session to ctx

---
+++ web/http_ctx.hpp
@@ -0,0 +1,78 @@
+#ifndef _RWG_WEB_HTTP_CTX_
+#define _RWG_WEB_HTTP_CTX_
+
+#include "web/abstract_in_event.hpp"
+#include "web/res.hpp"
+#include "web/req.hpp"
+#include "websocket/websocket_startup.hpp"
+#include <functional>
+#ifdef DEBUG
+#include <iostream>
+#endif
+
+namespace rwg_web {
+
+class http_ctx {
+private:
+    rwg_websocket::startup &_websocket;
+
+    std::function<void (rwg_web::req&, rwg_web::res&)> _http_handler;
+    std::function<void ()>_close_cb;
+    rwg_web::req _req;
+    rwg_web::res _res;
+public:
+    http_ctx(rwg_websocket::startup &websocket,
+             std::function<void (rwg_web::req& req, rwg_web::res&)> handler,
+             std::function<void ()> close_cb)
+        : _websocket(websocket)
+        , _http_handler(handler)
+        , _close_cb(close_cb) {}
+
+    rwg_web::req& req() { return this->_req; }
+    rwg_web::res& res() { return this->_res; }
+
+    void execute(int fd) {
+#ifdef DEBUG
+        std::cout << "session received message" << std::endl;
+#endif
+        this->_req.fd() = fd;
+        this->_res.fd() = fd;
+        if (bool(this->_http_handler)) {
+#ifdef DEBUG
+            std::cout << "execute start http_ctx init (parse)" << std::endl;
+#endif
+            this->_req.alloc_buf(512);
+            this->_req.parse();
+            if (this->_req.stat() == rwg_web::req_stat::req_stat_err ||
+                this->_req.stat() == rwg_web::req_stat::req_stat_interrupt) {
+                this->close();
+            }
+            else if (this->_req.stat() == rwg_web::req_stat::req_stat_header_end) {
+#ifdef DEBUG
+                std::cout << "execute start http_ctx run handle" << std::endl;
+#endif
+
+                this->_res.alloc_buf(512);
+                if (!this->_websocket.handshake(this->_req, this->_res)) {
+#ifdef DEBUG
+                    std::cout << "normal http" << std::endl;
+#endif
+                    this->_http_handler(this->_req, this->_res);
+                }
+                this->_res.free_buf();
+            }
+            this->_req.free_buf();
+        }
+    }
+
+    virtual ~http_ctx() {
+    }
+
+    void close() {
+        this->_close_cb();
+    }
+};
+
+}
+
+#endif
