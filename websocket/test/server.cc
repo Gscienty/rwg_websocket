c2676f4150aa1dd92bf49dd21d2ed9611a7039aa
高小川 <gaoxiaochuan@hotmail.com>
2018-10-27 12:15:03 +0800

    test websocket

--- websocket/test/server.cc
+++ websocket/test/server.cc
@@ -7,22 +7,10 @@
 #include <iostream>
 
 int main() {
-    rwg_websocket::startup websock;
     auto websock_handle = [] (rwg_websocket::frame &frame) -> void {
-        /* std::cout << frame.fin_flag() << std::endl; */
-        /* std::cout << frame.mask() << std::endl; */
-        /* std::cout << frame.masking_key() << std::endl; */
-        /* std::cout << frame.opcode() << std::endl; */
         std::string val(frame.payload().begin(), frame.payload().end());
         std::cout << val << std::endl;
-
-        rwg_websocket::frame resp_frame(frame.fd());
-
-        resp_frame.fin_flag() = true;
-        resp_frame.mask() = false;
-        resp_frame.opcode() = rwg_websocket::op::op_text;
     };
-    websock.set_handle(websock_handle);
 
     rwg_staticfile::startup staticfile("/static/", "./websocket/test/");
     staticfile.read_config("./staticfile/mime.conf");
@@ -34,34 +22,17 @@ int main() {
     server.listen("0.0.0.0", 5000);
 
     auto web_handle = [&] (rwg_web::req& req, rwg_web::res& res) -> void {
-        if (websock.is_websocket(req.fd())) {
-            websock.run(req.fd());
+        default_startup.run(req, res);
+
+        if (staticfile.run(req, res)) {
+            return;
         }
-        else {
-            req.alloc_buf(512);
-            res.alloc_buf(512);
-            req.parse();
 
-            default_startup.run(req, res);
-
-            if (websock.handshake(req, res)) {
-                return;
-            }
-            if (staticfile.run(req, res)) {
-                return;
-            }
-
-            not_impl.run(req, res);
-        }
+        not_impl.run(req, res);
     };
 
-    auto close_handle = [&] (int fd) -> void {
-        websock.remove(fd);
-    };
-
-
-    server.run(web_handle);
-    server.close_handle(close_handle);
+    server.http_handle(web_handle);
+    server.websocket_heandle(websock_handle);
 
     server.start();
 
