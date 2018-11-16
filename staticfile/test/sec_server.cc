8183a035a5c3baca81e9ad2f19364508c47e2aa8
高小川 <gaoxiaochuan@hotmail.com>
2018-11-16 23:38:35 +0800

    test staticfile https server

---
+++ staticfile/test/sec_server.cc
@@ -0,0 +1,24 @@
+#include "web/server.hpp"
+#include "staticfile/staticfile_startup.hpp"
+#include <iostream>
+
+int main() {
+    rwg_staticfile::startup staticfile("/static/", "staticfile/test/");
+    staticfile.read_config("staticfile/mime.conf");
+
+    rwg_web::server server;
+
+    server.use_security();
+    server.init_ssl("./server.crt", "./server.key");
+
+    server.listen("0.0.0.0", 5000);
+
+    auto func = [&] (rwg_web::req& req, rwg_web::res& res, std::function<void ()>) -> void {
+        std::cout << "staticfile" << std::endl;
+        staticfile.run(req, res);
+    };
+
+    server.http_handle(func);
+    server.start();
+    return 0;
+}
