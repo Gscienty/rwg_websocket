33b04be595842761b70bdbf3561d40e83eb45f17
高小川 <gaoxiaochuan@hotmail.com>
2018-11-16 23:38:05 +0800

    change test server port

--- web/test/server_test.cc
+++ web/test/server_test.cc
@@ -2,7 +2,7 @@
 
 int main() {
     rwg_web::server server(10, 1, 2000);
-    server.listen("127.0.0.1", 8088);
+    server.listen("0.0.0.0", 5000);
     auto exec = [] (rwg_web::req&, rwg_web::res& res, std::function<void ()>) -> void {
         res.version() = "HTTP/1.1";
         res.status_code() = 200;
