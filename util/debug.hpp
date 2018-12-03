9ba8b9d8515b34439cbba894076fdf59bf336ff4
高小川 <gaoxiaochuan@hotmail.com>
2018-12-03 14:11:37 +0800

    Create debug.hpp

---
+++ util/debug.hpp
@@ -0,0 +1,32 @@
+#ifndef _RWG_UTIL_DEBUG_
+#define _RWG_UTIL_DEBUG_
+
+#ifdef DEBUG
+#include <stdio.h>
+
+#define CL_COLOR_RED    "\033[1;31m"
+#define CL_COLOR_WHITE  "\033[1;37m"
+#define CL_COLOR_YELLOW "\033[1;33m"
+
+#define info(s, args...) \
+    fprintf(stderr, \
+            CL_COLOR_WHITE "INFO [file: \"" __FILE__"\". line: %i]" s CL_COLOR_WHITE "\n", \
+            __LINE__, ##args)
+
+#define warn(s, args...) \
+    fprintf(stderr, \
+            CL_COLOR_YELLOW "WARN [file: \"" __FILE__"\". line: %i]" s CL_COLOR_WHITE "\n", \
+            __LINE__, ##args)
+
+#define error(s, args...) \
+    fprintf(stderr, \
+            CL_COLOR_RED "INFO [file: \"" __FILE__"\". line: %i]" s CL_COLOR_WHITE "\n", \
+            __LINE__, ##args)
+#else
+#define info(s, ...)
+#define warn(s, ...)
+#define error(s, ...)
+#endif
+
+
+#endif
